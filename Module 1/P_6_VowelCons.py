1760472d701ec4e74fd6c3c6c8c773e1128d4bc4
Abrar Shaikh <sheikhabrar054@gmail.com>
2024-05-14 00:43:55 +0530

    Delete Module 1 directory

--- Module 1/P_6_VowelCons.py
+++
@@ -1,9 +0,0 @@
-ch = input("Enter any alphabet from A to Z:")
-
-if ch in('a','e','i','o','u','A','E','I','O','U'):
-
-    print(ch,"is vowel..")
-
-else:
-
-    print(ch,"is consonent..")
--- Module 1/P_9_SumNum.py
+++
@@ -1,5 +0,0 @@
-n = int(input("Enter positive number:"))
-
-sum = (n*(n+1)/2)
-
-print("Sum of the first",n,"positive numbers is",sum)
